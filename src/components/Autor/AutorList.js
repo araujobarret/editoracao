f3a18c7a7b385d5cb36b4768c67eba8cab31003e
Carlos Alberto <araujobarret@gmail.com>
2018-04-20 16:50:48 -0300

    Changes on the table of Autor list, started the edit dialog. The dialog can be granularized in the future for the purpose of creation a custom dialog for other models

--- src/components/Autor/AutorList.js
+++ src/components/Autor/AutorList.js
@@ -9,6 +9,13 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import Badge from 'material-ui/Badge';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import ModeEditIcon from 'material-ui/svg-icons/editor/mode-edit';
 
 import { startGetAutores } from '../../actions/AutorActions';
 import { Loader } from '../util/Loader';
@@ -18,7 +25,12 @@ class AutorList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { isLoading: true, };
+    this.state = {
+      editDialogOpen: false,
+      isLoading: true,
+      autor: null
+    };
+
   }
 
   componentWillReceiveProps(nextProps) {
@@ -32,25 +44,49 @@ class AutorList extends Component {
     dispatch(startGetAutores());
   }
 
+  onRowSelected = (row) => {
+    //row[0]
+  }
+
+  handleDialog = () => {
+    this.setState({editDialogOpen: !this.state.editDialogOpen});
+  }
+
+  openEditDialog = (autor) => {
+    this.setState({editDialogOpen: true, autor});
+  }
+
   _renderTable() {
     if( this.state.isLoading ) {
       return <Loader />;
     }
     else {
       const tableRows = this.props.autor.autores.map((autor) =>
-        <TableRow key={autor._id} style={{borderColor: "#ffffff"}}>
+        <TableRow key={autor._id} style={{borderColor: "#ffffff", overflow: "visible"}}>
           <TableRowColumn style={text}>{ autor.nome }</TableRowColumn>
+          <TableRowColumn style={{...text, ...actionText, overflow: "visible"}}>
+            <div className="tooltipContainer">
+              <IconButton tooltip="Editar Autor" onClick={() => this.openEditDialog(autor)}>
+                <ModeEditIcon color={"#e2ecf7"} hoverColor={"#ffffff"}/>
+              </IconButton>
+              <IconButton tooltip="Apagar Autor">
+                <DeleteIcon color={"#ff5252"} hoverColor={"#ffbaba"}/>
+              </IconButton>
+            </div>
+          </TableRowColumn>
         </TableRow>
       );
 
       return (
-        <Table style={{backgroundColor: "#1abc9c"}}>
-          <TableHeader displaySelectAll={false} adjustForCheckbox={false}  style={{border: "none"}}>
+        <Table style={{backgroundColor: "#1abc9c", overflow: "visible"}}
+          onRowSelection={this.onRowSelected} wrapperStyle={{paddingBottom: "20px", height: "88%"}}>
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false} style={{border: "none"}}>
             <TableRow style={{borderColor: "#ffffff"}}>
               <TableHeaderColumn style={header}>Nome do autor</TableHeaderColumn>
+              <TableHeaderColumn style={{...header, ...actionText}}>Ações</TableHeaderColumn>
             </TableRow>
           </TableHeader>
-          <TableBody displayRowCheckbox={false} >
+          <TableBody displayRowCheckbox={false} style={{overflow: "visible", paddingBottom: "40px"}}>
             { tableRows }
           </TableBody>
         </Table>
@@ -59,9 +95,36 @@ class AutorList extends Component {
   }
 
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancelar"
+        primary={true}
+        onClick={this.handleDialog}
+      />,
+      <FlatButton
+        label="Salvar"
+        primary={true}
+        onClick={this.handleDialog}
+      />,
+    ];
+
     return (
-      <section className="containerEntrada">
+      <section className="containerTable">
         { this._renderTable() }
+
+        <Dialog
+          title="Autor"
+          actions={actions}
+          modal={true}
+          open={this.state.editDialogOpen}>
+
+          <TextField
+            id="input"
+            value={this.state.autor.nome}
+            onChange={(e) => this.setState({autor: { ...this.state.autor, nome: e.target.value} })}
+            inputStyle={{color: "#2c3e50"}}/>
+
+        </Dialog>
       </section>
     );
   }
@@ -71,6 +134,10 @@ const text = {
   color: '#2c3e50',
   fontSize: '0.9em',
   fontRamily: 'Source Sans Pro',
+};
+
+const actionText = {
+  textAlign: "right"
 };
 
 const header = {
